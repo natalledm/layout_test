f56deb1493fa54dd3d438bf4ddee3bf2d1cf4b05
Natzsche <natallemoura7@gmail.com>
2019-04-08 17:40:48 +0200

    Final changes on the project :zap:

---
+++ main.js
@@ -0,0 +1,9 @@
+// Unfortunately this didn't make into the final project.
+
+// function openForm() {
+//     document.getElementById("Login").style.display = "block";
+// }
+
+// function closeForm() {
+//     document.getElementById("Login").style.display = "none";
+// }
